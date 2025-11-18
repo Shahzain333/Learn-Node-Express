9d1dfe4dfd6c866b1a9c1391306f0a5e100b3965
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2025-11-18 17:11:30 +0500

    Hello World & Module

--- 01 Hello-World/hello.js
+++ 01 Hello-World/hello.js
@@ -1,3 +1,9 @@
+const math = require('./math');
+
+// Do Destructuring
+//const {addFn, subFn} = require('./math');
+
+
 console.log("Hello World!")
 
 // V8 Engine directly embedede nahi hai node js mai bht si changes hain just like below 
@@ -6,11 +12,15 @@ console.log("Hello World!")
 // ki core functionallity hai bss
 //console.log(window);
 
+// Give Empty Object
+//console.log(math);
 
+//console.log("Math Valye is :", math)
 
+//console.log("Math Valye is :", math(2,4))
 
+// console.log("Additon is :", math.addFn(2,4))
+// console.log("Subtraction is :", math.subFn(4,2))
 
-
-
-
-
+console.log("Additon is :", math.addFn(2,4))
+console.log("Subtraction is :", math.subFn(4,2))
---
+++ 01 Hello-World/math.js
@@ -0,0 +1,23 @@
+// function add(a,b){
+//     return a + b;
+// }
+
+// function sub(a,b){
+//     return a - b;
+// }
+
+// //module.exports = "Shahzain"
+// module.exports = add;
+
+// module.exports = {
+//     addFn: add, 
+//     subFn: sub
+// }
+
+
+// 2nd Methods
+exports.addFn = (a,b) => a + b;
+
+exports.subFn = (a,b) => a - b;
+
+
