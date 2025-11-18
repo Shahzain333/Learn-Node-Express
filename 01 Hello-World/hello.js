968081f51533acb8d83d8df68312aad39ddd8899
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2025-11-18 12:33:36 +0500

    01 Hello World

---
+++ 01 Hello-World/hello.js
@@ -0,0 +1,16 @@
+console.log("Hello World!")
+
+// V8 Engine directly embedede nahi hai node js mai bht si changes hain just like below 
+
+// Window aur Dom ki tarah ki cheezain node pr available nahi hoti Is mai Js 
+// ki core functionallity hai bss
+//console.log(window);
+
+
+
+
+
+
+
+
+
