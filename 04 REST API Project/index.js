5a97ff42e6af58df1bbbecb2ad898385db7cde4f
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2025-11-20 15:23:50 +0500

    Using Postman Doing CRUD Operation get, post, patch, delete

--- 04 REST API Project/index.js
+++ 04 REST API Project/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const users = require('./MOCK_DATA.json')
+const fs = require('fs')
+let users = require('./MOCK_DATA.json')
 
 const app = express()
 const PORT = 3000
+
+// Middleware to parse JSON bodies - Plugin
+app.use(express.urlencoded({ extended: false })); // For parsing application/x-www-form-urlencoded
 
 // ----------------- Routes here -----------------
 
@@ -29,10 +33,22 @@ app.get('/api/users', (req,res) => {
 //     return res.json(user)
 // })
 
-app.post('/api/users', (req, res) => {
-    // TODO Create New User
-    return res.json({ status: 'Pending...' })
-})
+// app.post('/api/users', (req, res) => {
+//     // TODO Create New User
+//     const body = req.body; // Jo Bhi Frontend sy data recieve hota hai wo yahan ayega body pr
+    
+//     //console.log("Body", body);
+    
+//     users.push({id: users.length + 1, ...body }); // Adding new user to the users array
+
+//     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+//         if(err) {
+//             console.log("Error writing file", err);
+//         }
+//         return res.json({ status: 'Success...', id: users.length })
+//     })
+
+// })
 
 // app.patch('/api/users/:id', (req, res) => {
 //     // TODO Edit the User with ID
@@ -44,18 +60,84 @@ app.post('/api/users', (req, res) => {
 //     return res.json({ status: 'Pending...' })
 // })
 
-app.route('/api/users/:id').get((req,res) => {
-    const id = Number(req.params.id);  
-    const user = users.find((user) => user.id === id)
-    return res.json(user)
-}).put((req, res) => {
-    // Edit the User with ID
-    return res.json({ status: 'Pending...' })
-}).delete((req, res) => {
-    // Delete the User with ID
-    return res.json({ status: 'Pending...' })
+app.post('/api/users', (req, res) => {
+    // TODO Create New User
+    const body = req.body; // Jo Bhi Frontend sy data recieve hota hai wo yahan ayega body pr
+    
+    //console.log("Body", body);
+    
+    users.push({id: users.length + 1, ...body }); // Adding new user to the users array
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        if(err) {
+            console.log("Error writing file", err);
+        }
+        return res.json({ status: 'Success...', id: users.length })
+    })
+
 })
 
+app.route('/api/users/:id').get((req,res) => {
+
+    const id = Number(req.params.id);  
+    const user = users.find((user) => user.id === id)
+
+    // Check if user exists
+    const userExisting = users.some(user => user.id === id);
+    if(!userExisting){
+        return res.status(404).json({ status: `No user found with the id of ${id}` });
+    }
+    
+    return res.json(user)
+
+}).patch((req, res) => {
+
+    // Edit the User with ID
+    const id = Number(req.params.id);  
+    const user = users.find((user) => user.id === id)
+    const body = req.body;
+
+    for(let key in body){
+        user[key] = body[key];
+    }
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        if(err) {
+            console.log("Error Updating file", err);
+        }
+
+        return res.json({ id: users.length , status: 'Update Success...' })
+
+    })
+
+}).delete((req, res) => {
+
+    // Delete the User with ID
+    const id = Number(req.params.id);
+    
+    //const user = users.find((user) => user.id === id);  // Find user object
+    //const index = users.indexOf(user); // Find array position of that user
+    //users.splice(index, 1); // Remove 1 element at that position
+
+    // Alternative way to delete user
+
+    // Check if user exists
+    const userExisting = users.some(user => user.id === id);
+    if(!userExisting){
+        return res.status(404).json({ status: `No user found with the id of ${id}` });
+    }
+
+    users = users.filter(user => user.id !== id); // Create new array without the user
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        if(err) {
+            console.log("Error Deleting file", err);
+        }
+
+        return res.json({ id: users.length + 1 ,status: 'Delete Success...' })
+    })
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
